cdcd54bcdf88ed7b58d881170929895ebc279c7c
ashutosh8547 <ashutosh8547@gmail.com>
2025-07-12 14:58:29 +0530

    react hello world project

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement(
+    "div", {"id":"parent"}, [React.createElement(
+"div", {"id":"child1"},  [React.createElement("h1",{"id":"heading"},"Hello From react h1")
+    , React.createElement("h2",{"id":"heading2"},"Hello From react h2")
+]
+),
+React.createElement("div", {"id":"child2"},  
+    [React.createElement("h3",{"id":"heading3"},"Hello From react h3")
+    , React.createElement("h4",{"id":"heading4"},"Hello From react h4")
+]
+) ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
